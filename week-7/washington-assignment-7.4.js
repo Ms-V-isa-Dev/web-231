ed72ff05d15de0cbb2b43199ae5e2195203e65a7
Verlee Washington <VWashington@my365.bellevue.edu>
2020-02-02 12:58:54 -0600

    Turning in assignments

---
+++ week-7/washington-assignment-7.4.js
@@ -0,0 +1,22 @@
+/*
+============================================
+; Title: washington-discussion-7.4
+; Author: Professor Krasso
+; Date:   22 January 2020
+; Modified by: Verlee Washington
+; Description: Create a simple HTML page with
+; embedded JavaScript with 2 JavaScript
+; selector errors.
+;===========================================
+*/
+
+// Call the console.log() function to output a well-formatted header.
+const header = require('../Washington-header.js');
+
+
+
+
+
+
+
+
